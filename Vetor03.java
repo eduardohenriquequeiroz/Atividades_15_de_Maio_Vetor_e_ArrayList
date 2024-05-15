976a58e91bfa7c3e19d78ef2d1ac81850084c282
eduardohenriquequeiroz <eduardocanova77@gmail.com>
2024-05-15 12:12:21 -0300

    Primeiro commit

---
+++ Vetor03.java
@@ -0,0 +1,25 @@
+package vetoresEarraylist;
+
+import java.util.Scanner;
+
+public class Vetor03 {
+
+
+	public static void main(String[] args) {
+
+		String a[] = new String[5];
+
+		Scanner ler = new Scanner(System.in);
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Informe um nome " + i + "..i");
+			a[i] = ler.nextLine();
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println(a[i]);
+		}
+		ler.next();
+
+	}
+
+}
